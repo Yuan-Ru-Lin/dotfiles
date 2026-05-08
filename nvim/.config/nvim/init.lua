be619d7a5249a4dad6b61d9aae783933829a4b73
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2026-05-07 20:09:09 -0700

    Resolve conflict

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,6 @@
+-- ============================================================
+-- Options
+-- ============================================================
 local opt = vim.opt
 opt.tabstop     = 4
 opt.shiftwidth  = 4
@@ -6,6 +9,9 @@ opt.expandtab   = true
 opt.splitright  = true
 opt.splitbelow  = true
 
+-- ============================================================
+-- Global variables (plugin configs)
+-- ============================================================
 local g = vim.g
 g.slime_target                  = "neovim"
 g.AutoPairs                     = {
@@ -28,136 +34,174 @@ g.clipboard = {
   },
 }
 
--- Bootstrap packer.nvim if not installed
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-      'git', 'clone', '--depth', '1',
-      'https://github.com/wbthomason/packer.nvim',
-      install_path
-    })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
+-- ============================================================
+-- Plugins (vim.pack)
+-- ============================================================
+
+-- Plugins that load in both Neovim and VSCode extension
+vim.pack.add({
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/jpalardy/vim-slime",
+  "https://github.com/jiangmiao/auto-pairs",
+  "https://github.com/tpope/vim-surround",
+  "https://github.com/navarasu/onedark.nvim",
+  "https://github.com/raivivek/vim-snakemake",
+  "https://github.com/JuliaEditorSupport/julia-vim",
+  "https://github.com/vim-scripts/indentpython.vim",
+  "https://github.com/chomosuke/typst-preview.nvim",
+  "https://github.com/kana/vim-textobj-user",
+  "https://github.com/sgur/vim-textobj-parameter",
+  "https://github.com/lucapette/vim-textobj-underscore",
+  "https://github.com/fvictorio/vim-textobj-backticks",
+  "https://github.com/glts/vim-textobj-comment",
+  "https://github.com/vimlab/split-term.vim",
+  "https://github.com/tpope/vim-fugitive",
+})
+
+-- Plugins that should NOT load in VSCode extension
+if not vim.g.vscode then
+  vim.pack.add({
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/stevearc/conform.nvim",
+    "https://github.com/williamboman/mason.nvim",
+    "https://github.com/williamboman/mason-lspconfig.nvim",
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/greggh/claude-code.nvim",
+    "https://github.com/nvim-tree/nvim-tree.lua",
+    "https://github.com/nvim-tree/nvim-web-devicons",
+  })
 end
 
-local packer_bootstrap = ensure_packer()
-
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {'neovim/nvim-lspconfig', disable = vim.g.vscode}
-
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").setup({
-        install_dir = vim.fn.stdpath("data") .. "/site",
-      })
-      require("nvim-treesitter").install({ "typst" })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "typst" },
-        callback = function()
-          vim.treesitter.start()
-        end,
-      })
-    end,
-  })
-
-  use {
-    "stevearc/conform.nvim",
-    config = function()
-      require("conform").setup()
-    end,
-    disable = vim.g.vscode,
-  }
-
-  -- REPL & workflow
-  use 'jpalardy/vim-slime'
-
-  -- Auto-pairs & surrounding
-  use 'jiangmiao/auto-pairs'
-  use 'tpope/vim-surround'
-
-  use {
-    'navarasu/onedark.nvim',
-    config = function()
-      require('onedark').setup { style = 'darker' }
-      require('onedark').load()
-    end,
-  }
-
-  use 'raivivek/vim-snakemake'
-
-  use {
-    'JuliaEditorSupport/julia-vim',
-    config = function()
-      -- load matchit for % matching in Julia
-      vim.cmd 'runtime macros/matchit.vim'
+-- ============================================================
+-- Post-install / post-update hooks
+-- ============================================================
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    local name = ev.data.spec.name
+    if name == "nvim-treesitter" then
+      pcall(function() require("nvim-treesitter").update() end)
+    elseif name == "mason.nvim" then
+      pcall(vim.cmd, "MasonUpdate")
     end
-  }
+  end,
+})
 
-  use 'vim-scripts/indentpython.vim'
+-- ============================================================
+-- Plugin setups
+-- ============================================================
 
-  -- Typst
-  use 'chomosuke/typst-preview.nvim'
-
-  -- text-objects
-  use 'kana/vim-textobj-user'
-  use 'sgur/vim-textobj-parameter'
-  use {
-    'lucapette/vim-textobj-underscore',
-    requires = { 'kana/vim-textobj-user' },
-  }
-  
-  use {
-    'fvictorio/vim-textobj-backticks',
-    requires = { 'kana/vim-textobj-user' },
-  }
-  use 'glts/vim-textobj-comment'
-
-  use 'vimlab/split-term.vim'
-  use 'tpope/vim-fugitive'
-
-  use {
-    'williamboman/mason.nvim',
-    -- optional but recommended: keep Mason’s registry up-to-date
-    run = function() pcall(vim.cmd, 'MasonUpdate') end,
-    disable = vim.g.vscode,
-  }
-
-  -- (optionally, to bridge Mason with lspconfig)
-  use {
-    'williamboman/mason-lspconfig.nvim',
-    disable = vim.g.vscode
-  }
-
-  use {
-    'greggh/claude-code.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim', -- Required for git operations
-    },
-    config = function()
-      require('claude-code').setup()
-    end
-  }
-
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-  }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+-- Colorscheme (load early so subsequent highlights apply)
+pcall(function()
+  require("onedark").setup({ style = "darker" })
+  require("onedark").load()
 end)
 
+local function typst_hl()
+  local set = vim.api.nvim_set_hl
+
+  -- 標題分層：用同色系不同亮度做出階層感
+  set(0, "@markup.heading.1.typst", { fg = "#e06c75", bold = true })
+  set(0, "@markup.heading.2.typst", { fg = "#d19a66", bold = true })
+  set(0, "@markup.heading.3.typst", { fg = "#e5c07b", bold = true })
+  set(0, "@markup.heading.4.typst", { fg = "#98c379", bold = true })
+  set(0, "@markup.heading.5.typst", { fg = "#56b6c2", bold = true })
+  set(0, "@markup.heading.6.typst", { fg = "#61afef", bold = true })
+
+  -- 讓粗體、斜體真的變粗斜（而不是只換顏色）
+  set(0, "@markup.strong.typst",  { bold = true })
+  set(0, "@markup.italic.typst",  { italic = true })
+  set(0, "@markup.underline.typst", { underline = true })
+  set(0, "@markup.strikethrough.typst", { strikethrough = true })
+
+  -- raw code 塊用稍深的背景區隔
+  set(0, "@markup.raw.typst",       { fg = "#98c379" })
+  set(0, "@markup.raw.block.typst", { fg = "#abb2bf", bg = "#2c313a" })
+
+  set(0, "@markup.link.label.typst", { fg = "#61afef", underline = true })
+  set(0, "@markup.link.url.typst",   { fg = "#56b6c2", italic = true })
+  set(0, "@markup.list.typst",       { fg = "#e06c75" })
+
+  -- math 模式：物理系寫 Typst 的重點
+  set(0, "@markup.math.typst", { fg = "#56b6c2" })
+  set(0, "@function.typst",         { fg = "#61afef" })       -- #rect(...)、#set(...)
+  set(0, "@function.builtin.typst", { fg = "#56b6c2", italic = true })
+  set(0, "@keyword.typst",          { fg = "#c678dd", italic = true })
+  set(0, "@variable.typst",         { fg = "#e06c75" })
+  set(0, "@label.typst",            { fg = "#e5c07b" })       -- <eq:foo>
+  set(0, "@punctuation.special.typst", { fg = "#d19a66" })    -- #、$、@
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "onedark",
+  callback = typst_hl,
+})
+typst_hl()  -- 如果 colorscheme 已經載入了，立即套用一次
+
+-- Treesitter
+pcall(function()
+  require("nvim-treesitter").setup({
+    install_dir = vim.fn.stdpath("data") .. "/site",
+  })
+  require("nvim-treesitter").install({ "typst" })
+end)
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "typst" },
+  callback = function() vim.treesitter.start() end,
+})
+
+-- julia-vim needs matchit loaded
+vim.cmd("runtime macros/matchit.vim")
+
+-- nvim-tree
+pcall(function()
+  require("nvim-tree").setup({ view = { width = 50 } })
+end)
+
+-- Non-VSCode-only plugin setups
+if not vim.g.vscode then
+  pcall(function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "pyright", "tinymist" },
+    })
+
+    require("conform").setup({
+      formatters = {
+        runic = {
+          command = "julia",
+          args = { "--project=@runic", "-e", "using Runic; exit(Runic.main(ARGS))" },
+        },
+      },
+      formatters_by_ft = {
+        julia = { "runic" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        sql = { "sql_formatter" },
+      },
+      default_format_opts = {
+        timeout_ms = 10000,
+      },
+    })
+
+    require("claude-code").setup()
+  end)
+
+  vim.keymap.set("n", "<leader>f", function()
+    require("conform").format({
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 3000,
+    })
+  end, { desc = "Format buffer with Conform" })
+end
+
+-- ============================================================
+-- Keymaps
+-- ============================================================
 local km = vim.keymap.set
 km('n', '<leader>ev', ':vsplit $MYVIMRC<CR>', { noremap = true, silent = true })
 km('n', '<leader>sv', ':source $MYVIMRC<CR>', { noremap = true, silent = true })
@@ -168,6 +212,9 @@ km('t', '<S-Enter>', '<Enter>', { noremap = true })
 km('x', '*', [[y/\V<C-R>=escape(@", '/\')<CR><CR>]], { desc = 'Search selection forward' })
 km('x', '#', [[y?\V<C-R>=escape(@", '?\')<CR><CR>]], { desc = 'Search selection backward' })
 
+-- ============================================================
+-- Autocmds & commands
+-- ============================================================
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'python',
   callback = function()
@@ -175,47 +222,6 @@ vim.api.nvim_create_autocmd('FileType', {
     km('i', '<F9>', '<ESC>:wa<CR>:!%:p<CR>', { buffer = true, noremap = true })
   end,
 })
-
-if not vim.g.vscode then
-
-    require('mason').setup()
-    require('mason-lspconfig').setup {
-      ensure_installed = {
-          'pyright',
-          -- 'julials',
-          'tinymist',
-      },
-    }
-
-    require("conform").setup({
-        formatters = {
-            runic = {
-                command = "julia",
-                args = {"--project=@runic", "-e", "using Runic; exit(Runic.main(ARGS))"},
-            },
-        },
-        formatters_by_ft = {
-            julia = {"runic"},
-            c = {"clang-format"},
-            cpp = {"clang-format"},
-            sql = {"sql_formatter"},
-        },
-        default_format_opts = {
-            -- Increase the timeout in case Runic needs to precompile
-            -- (e.g. after upgrading Julia and/or Runic).
-            timeout_ms = 10000,
-        },
-    })
-
-    vim.keymap.set("n", "<leader>f", function()
-      require("conform").format({
-        lsp_fallback = true,
-        async = false,      -- block until formatting is done
-        timeout_ms = 3000,  -- fail if formatting takes more than 3s
-      })
-    end, { desc = "Format buffer with Conform" })
-
-end
 
 vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = "scp://*",
